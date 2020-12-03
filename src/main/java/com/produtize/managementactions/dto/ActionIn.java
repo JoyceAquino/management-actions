74acbeab52753764142ddd9cf8fcaef16dabab57
joyce.aquino <joyce.aquino@b2wdigital.com>
2020-12-02 23:51:37 -0300

    [new] Mapeando Dto

--- src/main/java/com/produtize/managementactions/dto/ActionIn.java
+++ src/main/java/com/produtize/managementactions/dto/ActionIn.java
@@ -1,5 +1,34 @@
 package com.produtize.managementactions.dto;
 
+import com.produtize.managementactions.model.Action;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import org.springframework.lang.NonNull;
+
+
+import javax.validation.constraints.DecimalMin;
+import java.util.Date;
+
+
+@Data
+@AllArgsConstructor
 public class ActionIn {
+
+    @NonNull
+    @DecimalMin(value = "0.01")
+    private Double BuyPrice;
+
+    @NonNull
+    @DecimalMin(value = "0.01")
+    private Double SalePrice;
+
+    @Builder.Default
+    private Action.Currency currency = Action.Currency.BRL;
+
+    private int Quantity;
+    private Date SaleDate;
+    private String Active;
+
 
 }
