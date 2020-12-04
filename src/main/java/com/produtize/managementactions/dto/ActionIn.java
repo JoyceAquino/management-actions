e66b391d2ffd4fce4c7823a4abdd7220dce11387
joyce.aquino <joyce.aquino@b2wdigital.com>
2020-12-04 13:11:28 -0300

    [new] Ajustando as variáveis do Dto

--- src/main/java/com/produtize/managementactions/dto/ActionIn.java
+++ src/main/java/com/produtize/managementactions/dto/ActionIn.java
@@ -6,7 +6,6 @@ import lombok.Builder;
 import lombok.Data;
 import org.springframework.lang.NonNull;
 
-
 import javax.validation.constraints.DecimalMin;
 import javax.validation.constraints.NotBlank;
 import java.util.Date;
@@ -18,24 +17,23 @@ public class ActionIn {
 
     @NonNull
     @DecimalMin(value = "0.01")
-    private Double BuyPrice;
+    private Double buyPrice;
 
     @NonNull
     @DecimalMin(value = "0.01")
-    private Double SalePrice;
+    private Double salePrice;
 
     @Builder.Default
     private Action.Currency currency = Action.Currency.BRL;
 
     @DecimalMin(value = "1")
-    private int Quantity;
+    private int quantity;
 
-    @NonNull
-    private Date SaleDate;
+    private Date saleDate;
 
     @NonNull
     @NotBlank
-    private String Active;
+    private String active;
 
 
 }
